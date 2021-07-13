0d17c9f782b826f6ef71ba7fcef8d6ef0d073ba9
Khondekar Mustak Riyaj <57648835+mustakriyaj@users.noreply.github.com>
2021-07-13 12:37:04 +0530

    temporal_deletion

--- Angular/src/app/components/login/src/main/java/com/example/login/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.login;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, String> {
-
-}
--- Angular/src/app/components/models/address.ts
+++
@@ -1,9 +0,0 @@
-export class Address {
-    buildingName: string|undefined;
-    streetNo: string|undefined;
-    city: string|undefined;
-    state: string|undefined;
-    country: string|undefined;
-    pincode: string|undefined;
-    customerId: number|undefined;
-}
